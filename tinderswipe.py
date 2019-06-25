fd288ac6d63c38fe1a874f52c5b4c210a054e042
Shreyan Ganguly <52214843+GangulyShreyan@users.noreply.github.com>
2019-06-26 05:17:20 +0530

    Update tinderswipe.py

--- tinderswipe.py
+++ tinderswipe.py
@@ -1,17 +1,16 @@
 import pyautogui
 
-#taking screenshot
 image_data = pyautogui.screenshot()
 
 huntcomplete = 10
 counter = 0
 
 while(True):
-    #move cursor to desired pos.
+    
     pyautogui.moveTo(720,483)
-    #draggg
     pyautogui.dragTo(1150,414,1.5,button='left')
     
     counter = counter + 1
     if(counter == huntcomplete):
         break
+quit() 
