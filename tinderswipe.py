28ce76b920257c3ae1851dc7709328e2e5c3c3e3
Shreyan Ganguly <52214843+GangulyShreyan@users.noreply.github.com>
2019-06-25 23:50:49 +0530

    Add files via upload

---
+++ tinderswipe.py
@@ -0,0 +1,15 @@
+
+import pyautogui
+#taking screenshot
+im=pyautogui.screenshot()
+huntcomplete=10
+counter=0
+while(True):
+    #move cursor to desired pos.
+    pyautogui.moveTo(720,483)
+    #draggg
+    pyautogui.dragTo(1150,414,1.5,button='left')
+    #huntcomplete=huntcomplete+1
+    counter=counter+1
+    if(counter==huntcomplete):
+        break
